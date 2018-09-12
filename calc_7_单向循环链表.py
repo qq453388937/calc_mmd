2b9916a9dd273a7c0de408ccd943268686c3a988
camelia-peixiaodong <453388937@qq.com>
2018-09-12 16:20:51 +0800

    update

--- calc_7_单向循环链表.py
+++ calc_7_单向循环链表.py
@@ -8,13 +8,27 @@ class BaseNode(object):
 class SingleCircleList(object):
     """ 单向循环列表 """
     def __init__(self, node=None):
+        """ 初始化头元素,从头开始查找 """
         self.__head = node
 
     def is_empty(self):
         """ 判空 """
         return self.__head is None
 
+    def length(self):
+        """ 求单向循环列表长度 """
+        cur = self.__head
+        if self.is_empty():
+            return 0
+        count = 1
+        while cur.next is not self.__head:
+            """进入循环统计元素个数"""
+            count += 1
+        # 循环结束少加1个前面补偿进去
+        return count
+
 
 if __name__ == '__main__':
     sc = SingleCircleList()
     print(sc.is_empty())
+    print(sc.length())
